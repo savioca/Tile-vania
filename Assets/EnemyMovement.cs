b6ef4677b687b436c8c1814153aa8bb539d37c4b
SAVIO ROSA OLIVEIRA <saviorosaoliveira@gmail.com>
2020-06-26 18:11:28 -0300

    enemy 2/2

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
     [SerializeField] float moveSpeed = 1f;
     Rigidbody2D myRigidBody;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
@@ -16,6 +17,23 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        if (IsFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
     }
 }
